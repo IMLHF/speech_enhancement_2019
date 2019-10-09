00c78bf1c7f2e2840b8a6ea3ddfa10396a8c2663
IMLHF <red_wind@foxmail.com>
2019-10-09 13:08:46 +0800

    update print_log

--- nn_se/utils/misc_utils.py
+++ nn_se/utils/misc_utils.py
@@ -32,33 +32,36 @@ def exp_configName_dir():
 
 
 def noam_scheme(init_lr, global_step, warmup_steps=4000.):
-    '''Noam scheme learning rate decay
-    init_lr: initial learning rate. scalar.
-    global_step: scalar.
-    warmup_steps: scalar. During warmup_steps, learning rate increases
-        until it reaches init_lr.
-    '''
-    step = tf.cast(global_step + 1, dtype=tf.float32)
-    return init_lr * warmup_steps ** 0.5 * tf.minimum(step * warmup_steps ** -1.5, step ** -0.5)
+  '''Noam scheme learning rate decay
+  init_lr: initial learning rate. scalar.
+  global_step: scalar.
+  warmup_steps: scalar. During warmup_steps, learning rate increases
+      until it reaches init_lr.
+  '''
+  step = tf.cast(global_step + 1, dtype=tf.float32)
+  return init_lr * warmup_steps ** 0.5 * tf.minimum(step * warmup_steps ** -1.5, step ** -0.5)
 
 
 def show_variables(vars_):
-    slim.model_analyzer.analyze_vars(vars_, print_info=True)
-    sys.stdout.flush()
+  slim.model_analyzer.analyze_vars(vars_, print_info=True)
+  sys.stdout.flush()
 
 
 def show_all_variables():
-    model_vars = tf.trainable_variables()
-    show_variables(model_vars)
+  model_vars = tf.trainable_variables()
+  show_variables(model_vars)
 
 
-def print_log(msg, log_file: str, no_time=False):
-    if not no_time:
-        time_stmp = "%s | " % time.ctime()
-        msg = time_stmp+msg
-    print(msg, end='', flush=True)
+def print_log(msg, log_file=None, no_time=False):
+  if log_file is not None:
+    log_file = str(log_file)
+  if not no_time:
+      time_stmp = "%s | " % time.ctime()
+      msg = time_stmp+msg
+  print(msg, end='', flush=True)
+  if log_file:
     with open(log_file, 'a+') as f:
-        f.write('msg')
+        f.write(msg)
 
 
 def check_tensorflow_version():
